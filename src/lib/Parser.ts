4a406b8ac83840d44fd775052a678e28c0473eae
rus_anonym <alexandrsemin2033@gmail.com>
2022-01-19 19:53:16 +0300

    add getSchedule method

--- src/lib/Parser.ts
+++ src/lib/Parser.ts
@@ -5,15 +5,28 @@ interface IParserOptions {
     host: string;
 }
 
+const pages = {
+    schedule: "/studentu/raspisanie-zanyatiy/",
+} as const;
+
 class Parser {
     private _host: string;
+    private _days = [
+        "Воскресенье",
+        "Понедельник",
+        "Вторник",
+        "Среда",
+        "Четверг",
+        "Пятница",
+        "Суббота",
+    ] as const;
 
     constructor({ host = "https://mpt.ru" }: Partial<IParserOptions> = {}) {
         this._host = host;
     }
 
     public async getCurrentWeek(): Promise<MPT.TWeek> {
-        const $ = await this._loadPage("/studentu/raspisanie-zanyatiy/");
+        const $ = await this._loadPage(pages.schedule);
         const parsedWeek = $("span.label").text().trim();
         if (/Знаменатель/i.test(parsedWeek)) {
             return "Знаменатель";
@@ -24,9 +37,148 @@ class Parser {
         }
     }
 
+    public async getSchedule(): Promise<MPT.Schedule.ISpecialty[]> {
+        const $ = await this._loadPage(pages.schedule);
+
+        const specialtyList: MPT.Schedule.ISpecialty[] = [];
+        const schedule = $("div.tab-content:nth-child(6)");
+
+        schedule.children().each((index, element) => {
+            const elem = $(element);
+            const specialty: MPT.Schedule.ISpecialty = {
+                name: elem
+                    .find("h2:nth-child(1)")
+                    .text()
+                    .trim()
+                    .replace("Расписание занятий для ", ""),
+                groups: [],
+            };
+
+            const specialtyGroups = elem.find(".tab-content").first();
+
+            specialtyGroups.children().each((index, element) => {
+                const elem = $(element);
+
+                const groupsNames = this._fixNonDecodeString(
+                    elem.find("h3").text().trim()
+                )
+                    .replace("Группа ", "")
+                    .split(", ");
+
+                const groupWeekSchedule: MPT.Schedule.IDay[] = [];
+
+                const weekSchedule = elem.find("table:nth-child(2)").children();
+                weekSchedule.each((index, element) => {
+                    const elem = $(element);
+                    if (elem.prop("name") === "tbody") {
+                        return;
+                    }
+
+                    const title = elem.find("h4");
+                    const placeName = title.find("span").text().trim();
+                    const dayName = title.text().replace(placeName, "").trim();
+
+                    const daySchedule: MPT.Schedule.IDay = {
+                        num: this._getDayNum(dayName),
+                        place: placeName.replace(/\(|\)/g, "") || "Отсутствует",
+                        lessons: [],
+                    };
+
+                    const schedule = elem.next().children();
+                    schedule.each((index, element) => {
+                        if (index === 0) {
+                            return;
+                        }
+
+                        const elem = $(element);
+                        const lessonNum = Number(
+                            elem.find("td:nth-child(1)").text()
+                        );
+
+                        if (lessonNum === 0) {
+                            return;
+                        }
+
+                        let lessonName: [string, string?];
+                        let teacherName: [string, string?];
+
+                        const lessonElement = elem.find("td:nth-child(2)");
+                        const teacherElement = elem.find("td:nth-child(3)");
+
+                        if (lessonElement.children().length === 0) {
+                            lessonName = [
+                                lessonElement.text().trim() || "Отсутствует",
+                            ];
+                            teacherName = [
+                                teacherElement.text().trim() || "Отсутствует",
+                            ];
+                        } else {
+                            lessonName = [
+                                lessonElement
+                                    .find("div:nth-child(1)")
+                                    .text()
+                                    .trim(),
+                                lessonElement
+                                    .find("div:nth-child(3)")
+                                    .text()
+                                    .trim(),
+                            ];
+
+                            teacherName = [
+                                teacherElement
+                                    .find("div:nth-child(1)")
+                                    .text()
+                                    .trim(),
+                                teacherElement
+                                    .find("div:nth-child(3)")
+                                    .text()
+                                    .trim(),
+                            ];
+                        }
+
+                        daySchedule.lessons.push({
+                            num: lessonNum,
+                            name: lessonName,
+                            teacher: teacherName,
+                        });
+                    });
+                    groupWeekSchedule.push(daySchedule);
+                });
+
+                specialty.groups.push(
+                    ...groupsNames.map((name) => {
+                        return { name, days: groupWeekSchedule };
+                    })
+                );
+            });
+
+            specialtyList.push(specialty);
+        });
+
+        return specialtyList;
+    }
+
     private _generateCookie(): string {
         const id = Math.random().toString(36).substring(2);
         return `PHPSESSID=MPT_Parser#${id};`;
+    }
+
+    private _fixNonDecodeString(input: string): string {
+        try {
+            return decodeURI(
+                input
+                    .replace("_2C ", ", ")
+                    .replace("_2F", "/")
+                    .replace(/_/gi, "%")
+            );
+        } catch (error) {
+            return input;
+        }
+    }
+
+    private _getDayNum(dayName: string): number {
+        const daysRegExps = this._days.map((x) => new RegExp(x, "gi"));
+        return daysRegExps.findIndex((x) => x.test(dayName));
     }
 
     private async _loadPage(path: string): Promise<CheerioAPI> {
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,6 +1,30 @@
 declare namespace MPT {
     type TWeek = "Знаменатель" | "Числитель";
 
+    namespace Schedule {
+        interface ILesson {
+            num: number;
+            name: [string, string?];
+            teacher: [string, string?];
+        }
+
+        interface IDay {
+            num: number;
+            place: string;
+            lessons: ILesson[];
+        }
+
+        interface IGroup {
+            name: string;
+            days: Day[];
+        }
+
+        interface ISpecialty {
+            name: string;
+            groups: Group[];
+        }
+    }
+
     namespace Replacements {
         interface ILesson {
             name: string;
