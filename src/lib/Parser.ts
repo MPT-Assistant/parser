3b2846e4e3a1730ed8487bc76b273cd7b7c37096
rus_anonym <rus_cybersec@vk.com>
2025-01-21 06:15:10 +0300

    feat: change 40.02.04 bypass

--- src/lib/Parser.ts
+++ src/lib/Parser.ts
@@ -347,11 +347,8 @@ class Parser {
     const oops = response.find((x) => x.code === "40.02.01");
     // In case they do add it to the list of specialties
     if (oops && response.some((x) => x.code === "40.02.04") === false) {
-      response.push({
-        ...oops,
-        code: "40.02.04",
-        name: oops.name.replace("40.02.01", "40.02.04"),
-      });
+      oops.code = "40.02.04, " + oops.code;
+      oops.code = oops.name.replace("40.02.01", "40.02.04, 40.02.01");
     }
 
     return response;
